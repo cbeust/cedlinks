dc2aa0b43ef7f3a5cd6f71fddb8fe60027e53a4b
Cedric Beust <cedric@beust.com>
2020-08-11 14:11:39 -0700

    submitLink

--- src/main/kotlin/com/beust/cedlinks/CedLinkResource.kt
+++ src/main/kotlin/com/beust/cedlinks/CedLinkResource.kt
@@ -22,4 +22,24 @@ class CedLinkResource {
     {
         return repository.listLinks(all, id)
     }
+
+    @GET
+    @Path("submit")
+    @Produces(MediaType.TEXT_HTML)
+    fun submitLink(
+            @QueryParam("url") url: String = "",
+            @QueryParam("title") title: String? = null,
+            @QueryParam("comment") comment: String? = null,
+            @QueryParam("time") time: String? = null
+    ): String
+    {
+        val result = Template.render("submitLink.mustache", mapOf(
+                "url" to url,
+                "comment" to comment,
+                "title" to title,
+                "host" to Config.host,
+                "time" to time
+        ))
+        return result
+    }
 }
